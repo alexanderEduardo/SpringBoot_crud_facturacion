75b74b7e9426b2c39a6c2a7fdc4f776692f11deb
Alexander <alexdev0411@gmail.com>
2022-05-18 07:34:35 -0500

    " I have created a Query Method to bring me the invoice entity with its related data. "

---
+++ src/main/java/com/alex/springboot/app/dao/IFacturaDao.java
@@ -0,0 +1,10 @@
+package com.alex.springboot.app.dao;
+
+import com.alex.springboot.app.models.entity.Factura;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IFacturaDao extends CrudRepository<Factura,Long> {
+    @Query("select f FROM Factura f join fetch f.cliente c join fetch f.items l join fetch l.producto where f.id=?1")
+    public Factura fetchByIdWithClienteWithItemFacturaWithProducto(Long id);
+}
