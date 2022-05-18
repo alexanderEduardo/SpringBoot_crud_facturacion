f6527ac9bc43a272e938460808a30b618a519496
Alexander <alexdev0411@gmail.com>
2022-05-17 20:38:55 -0500

    "  "

--- src/main/java/com/alex/springboot/app/models/services/ClienteServiceImplements.java
+++ src/main/java/com/alex/springboot/app/models/services/ClienteServiceImplements.java
@@ -2,6 +2,8 @@ package com.alex.springboot.app.models.services;
 
 import com.alex.springboot.app.dao.IClienteDao;
 import com.alex.springboot.app.models.entity.Cliente;
+import com.alex.springboot.app.models.entity.Factura;
+import com.alex.springboot.app.models.entity.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
@@ -13,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service("ClienteServiceImpls") //fascade Un unico punto de acceso a daos o repositorys . Aca podemos operar con diferentes clases Dao
-public class ClienteServiceImplements implements IClienteService{
+public class ClienteServiceImplements  implements IClienteService{
 
     /**Aca podriamos tener diferentes DAO y dentro del service accedemos a los distintos daos
      * con un unico punto de acceso . Aca en el service podemos manegar la transaccion sin tener que implementar
@@ -45,6 +47,31 @@ public class ClienteServiceImplements implements IClienteService{
     @Override @Transactional
     public void delete(Long id) {
         clienteDao.delete(id);
+    }
+
+    @Override @Transactional(readOnly = true)
+    public List<Producto> findByNombre(String term) {
+        return null;
+    }
+
+    @Override
+    public void saveFactura(Factura factura) {
+
+    }
+
+    @Override
+    public Producto findProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Factura findFacturabyId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteFactura(Long id) {
+
     }
 
 
