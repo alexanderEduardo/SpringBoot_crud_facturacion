409be89425b67a9ac8f6d72e18c63af2d63a8f5c
Alexander <alexdev0411@gmail.com>
2022-05-18 07:31:45 -0500

    "I have added several metodes to my main service layer"

--- src/main/java/com/alex/springboot/app/models/services/IClienteService.java
+++ src/main/java/com/alex/springboot/app/models/services/IClienteService.java
@@ -1,6 +1,8 @@
 package com.alex.springboot.app.models.services;
 
 import com.alex.springboot.app.models.entity.Cliente;
+import com.alex.springboot.app.models.entity.Factura;
+import com.alex.springboot.app.models.entity.Producto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,6 +13,13 @@ public interface IClienteService {
     public Page<Cliente> findAll(Pageable pageable);
     public void save(Cliente cliente);
     public Cliente findOne(Long id);
+    public Cliente fetchByIdWithFacturas(Long id);
     public void delete(Long id);
+    public List<Producto> findByNombre(String term);
    // public void auto_incremental();
+    public void saveFactura(Factura factura);
+    public Producto findProductById(Long id);
+    public Factura findFacturabyId(Long id);
+    public void deleteFactura(Long id);
+    public Factura fetchByIdWithClienteWithItemFacturaWithProducto(Long id);
 }
