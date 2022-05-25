a806852d4126ab546c496413897d2ee3c27cc9cb
Alexander <alexdev0411@gmail.com>
2022-05-24 19:17:30 -0500

    He modificado el LoginSuccessHandler para que el mensaje de bienvenida sea en el idioma seleccionado

--- src/main/java/com/alex/springboot/app/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/alex/springboot/app/auth/handler/LoginSuccessHandler.java
@@ -1,9 +1,12 @@
 package com.alex.springboot.app.auth.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 import javax.servlet.FilterChain;
@@ -11,16 +14,27 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Locale;
 
 @Component
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private LocaleResolver localeResolver;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
         FlashMap flashMap = new FlashMap();
-        flashMap.put("success","Has iniciado Sesion con exito "+authentication.getName()+"!!");
+        Locale locale = localeResolver.resolveLocale(request);
+        //flashMap.put("success","Has iniciado Sesion con exito "+authentication.getName()+"!!");
+        String message=messageSource.getMessage("text.login.success",null,locale);
+        flashMap.put("success" , message.concat(authentication.getName()));
+        System.out.println(authentication.getName());
         flashMapManager.saveOutputFlashMap(flashMap,request,response);
-        System.out.println("LOGIN SUCCESS");
+        System.out.println("~LOGIN SUCCESS~");
         System.out.println(this.getClass()+" "+"user? ");
         super.onAuthenticationSuccess(request, response, authentication);
     }
